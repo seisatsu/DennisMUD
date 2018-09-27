e7e6bcc5334c4a2b9f940a6dc4bb93724a867265
Michael D. Reiley <seisatsu@seisat.su>
2018-09-27 13:45:56 -0700

    Help message typo

--- commands/announce.py
+++ commands/announce.py
@@ -1,6 +1,6 @@
 NAME = "announce"
 USAGE = "announce <message>"
-DESCRIPTION = "Send a message to all users."
+DESCRIPTION = "(WIZARDS ONLY) Send a message to all users."
 
 
 def COMMAND(console, database, args):
