d756efaba0f9d0bb086bc1cd528eb2ab7b3d2257
Michael D. Reiley <seisatsu@seisat.su>
2018-09-28 16:23:54 -0700

    Private messages by username.

---
+++ commands/message.py
@@ -0,0 +1,22 @@
+NAME = "message"
+USAGE = "message <username> <message>"
+DESCRIPTION = "Send a message to the user <username>. Does not use nicknames."
+
+
+def COMMAND(console, database, args):
+    if len(args) < 2:
+        console.msg("Usage: " + USAGE)
+        return False
+
+    # Make sure we are logged in.
+    if not console.user:
+        console.msg(NAME + ": must be logged in first")
+        return False
+
+    for c in console.router.users:
+        if c.user["name"].lower() == args[0].lower():
+            c.msg("<<" + console.user["name"] + ">>: " + ' '.join(args[1:]))
+            return True
+
+    console.msg(NAME + ": no such user is logged in")
+    return True
