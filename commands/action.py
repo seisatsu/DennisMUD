fa62a043e45ccb6c2e6a24fe8b9a05b621fc3c5f
Michael D. Reiley <seisatsu@seisat.su>
2018-09-28 02:20:18 -0700

    action command

---
+++ commands/action.py
@@ -0,0 +1,18 @@
+NAME = "action"
+USAGE = "action <message>"
+DESCRIPTION = "Send a message styled as performing an action."
+
+
+def COMMAND(console, database, args):
+    if len(args) == 0:
+        console.msg("Usage: " + USAGE)
+        return False
+
+    # Make sure we are logged in.
+    if not console.user:
+        console.msg(NAME + ": must be logged in first")
+        return False
+
+    console.broadcast_room(console.user["nick"] + " " + ' '.join(args))
+
+    return True
