ac29b760ab92d760f9bc8d5faa64411c566b027a
Michael D. Reiley <seisatsu@seisat.su>
2018-10-01 17:53:35 -0700

    ...

--- websocket-backend.py
+++ websocket-backend.py
@@ -145,7 +145,7 @@ class ChatFactory(WebSocketServerFactory):
             if c['client-peer'] == peer:
                 client = c['client']
         if client:
-            client.sendMessage(payload.replace("\n", "<br/>"))
+            client.sendMessage(payload.encode().replace("\n", "<br/>").decode('utf-8'))
 
 
 if __name__ == "__main__":
