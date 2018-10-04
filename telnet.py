4205d9092e4ee3702613434455840fdeb9a193bc
Michael D. Reiley <seisatsu@seisat.su>
2018-10-03 17:08:22 -0700

    Begin consolidating telnet and websocket servers.

--- telnet.py
+++ telnet.py
@@ -25,82 +25,13 @@
 # IN THE SOFTWARE.
 # **********
 
-# Codebase borrowed from https://github.com/TKeesh/WebSocketChat
+# Parts of codebase borrowed from https://github.com/TKeesh/WebSocketChat
 
-import console
-import database
-import json
-import sys
-
-from twisted.python import log
-from twisted.internet import protocol, reactor
+from twisted.internet import protocol
 from twisted.protocols.basic import LineReceiver
 
-# Read the config file.
-with open("telnet.config.json") as f:
-    config = json.load(f)
 
-# Open the Dennis main database.
-dbman = database.DatabaseManager(config["database"]["host"], config["database"]["port"], config["database"]["name"])
-
-# Reset users.
-rooms = dbman.rooms.find()
-if rooms.count():
-    for r in rooms:
-        r["users"] = []
-        dbman.upsert_room(r)
-users = dbman.users.find()
-if users.count():
-    for u in users:
-        u["online"] = False
-        dbman.upsert_user(u)
-
-
-class Router:
-    def __init__(self):
-        self.users = {}
-        self.chatfactory = None
-
-    def __contains__(self, item):
-        if item in self.users:
-            return True
-        return False
-
-    def __getitem__(self, item):
-        if self.__contains__(item):
-            return self.users[item]
-        else:
-            return None
-
-    def __iter__(self):
-        return self.users.items()
-
-    def register(self, peer):
-        print(peer)
-        self.users[peer] = console.Console(dbman, peer, self)
-
-    def unregister(self, peer):
-        self.users[peer].command("logout")
-        del self.users[peer]
-
-    def message(self, peer, msg):
-        self.chatfactory.communicate(peer, msg.encode())
-
-    def broadcast_all(self, msg):
-        for u in self.users:
-            if not self.users[u].user:
-                continue
-            self.chatfactory.communicate(self.users[u].rname, msg.encode())
-
-    def broadcast_room(self, room, msg):
-        for u in self.users:
-            if not self.users[u].user:
-                continue
-            if self.users[u].user["room"] == room:
-                self.chatfactory.communicate(self.users[u].rname, msg.encode())
-
-
-class ChatProtocol(LineReceiver):
+class ServerProtocol(LineReceiver):
     def __init__(self, factory):
         self.factory = factory
         self.peer = None
@@ -122,23 +53,23 @@ class ChatProtocol(LineReceiver):
         self.factory.router[self.peer].command(line.decode())
 
 
-class ChatFactory(protocol.Factory):
+class ServerFactory(protocol.Factory):
 
     def __init__(self, router, *args, **kwargs):
         self.router = router
-        self.router.chatfactory = self
-        super(ChatFactory, self).__init__(*args)
+        self.router.telnet_factory = self
+        super(ServerFactory, self).__init__(*args)
         self.clients = []
 
     def buildProtocol(self, addr):
-        return ChatProtocol(self)
+        return ServerProtocol(self)
 
     def register(self, client):
         self.clients.append({'client-peer': client.peer, 'client': client})
-        router.register(client.peer)
+        self.router.register(client.peer)
 
     def unregister(self, client):
-        router.unregister(client.peer)
+        self.router.unregister(client.peer)
         for c in self.clients:
             if c['client-peer'] == client.peer:
                 self.clients.remove(c)
@@ -150,14 +81,3 @@ class ChatFactory(protocol.Factory):
                 client = c['client']
         if client:
             client.sendLine(payload)
-
-
-if __name__ == "__main__":
-    router = Router()
-
-    log.startLogging(sys.stdout)
-
-    factory = ChatFactory(router)
-
-    reactor.listenTCP(config["server"]["port"], ChatFactory(router))
-    reactor.run()
--- websocket.py
+++ websocket.py
@@ -25,81 +25,9 @@
 # IN THE SOFTWARE.
 # **********
 
-# Codebase borrowed from https://github.com/TKeesh/WebSocketChat
-
-import html
-import console
-import database
-import json
-import sys
-
-from twisted.python import log
-from twisted.internet import reactor
+# Parts of codebase borrowed from https://github.com/TKeesh/WebSocketChat
 
 from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol
-
-# Read the config file.
-with open("websocket.config.json") as f:
-    config = json.load(f)
-
-# Open the Dennis main database.
-dbman = database.DatabaseManager(config["database"]["host"], config["database"]["port"], config["database"]["name"])
-
-# Reset users.
-rooms = dbman.rooms.find()
-if rooms.count():
-    for r in rooms:
-        r["users"] = []
-        dbman.upsert_room(r)
-users = dbman.users.find()
-if users.count():
-    for u in users:
-        u["online"] = False
-        dbman.upsert_user(u)
-
-
-class Router:
-    def __init__(self):
-        self.users = {}
-        self.chatfactory = None
-
-    def __contains__(self, item):
-        if item in self.users:
-            return True
-        return False
-
-    def __getitem__(self, item):
-        if self.__contains__(item):
-            return self.users[item]
-        else:
-            return None
-
-    def __iter__(self):
-        return self.users.items()
-
-    def register(self, peer):
-        print(peer)
-        self.users[peer] = console.Console(dbman, peer, self)
-
-    def unregister(self, peer):
-        self.users[peer].command("logout")
-        del self.users[peer]
-
-    def message(self, peer, msg):
-        self.chatfactory.communicate(peer, html.escape(msg).encode("utf-8"))
-
-    def broadcast_all(self, msg):
-        for u in self.users:
-            if not self.users[u].user:
-                continue
-            self.chatfactory.communicate(self.users[u].rname, html.escape(msg).encode("utf-8"))
-
-    def broadcast_room(self, room, msg):
-        for u in self.users:
-            if not self.users[u].user:
-                continue
-            if self.users[u].user["room"] == room:
-                self.chatfactory.communicate(self.users[u].rname, html.escape(msg).encode("utf-8"))
 
 
 class ServerProtocol(WebSocketServerProtocol):
@@ -121,20 +49,20 @@ class ServerProtocol(WebSocketServerProtocol):
         self.factory.router[self.peer].command(payload.decode('utf-8'))
 
 
-class ChatFactory(WebSocketServerFactory):
+class ServerFactory(WebSocketServerFactory):
 
     def __init__(self, router, *args, **kwargs):
         self.router = router
-        self.router.chatfactory = self
-        super(ChatFactory, self).__init__(*args)
+        self.router.websocket_factory = self
+        super(ServerFactory, self).__init__(*args)
         self.clients = []
 
     def register(self, client):
         self.clients.append({'client-peer': client.peer, 'client': client})
-        router.register(client.peer)
+        self.router.register(client.peer)
 
     def unregister(self, client):
-        router.unregister(client.peer)
+        self.router.unregister(client.peer)
         for c in self.clients:
             if c['client-peer'] == client.peer:
                 self.clients.remove(c)
@@ -146,18 +74,3 @@ class ChatFactory(WebSocketServerFactory):
                 client = c['client']
         if client:
             client.sendMessage(payload.decode('utf-8').replace("\n", "<br/>").encode('utf-8'))
-
-
-if __name__ == "__main__":
-    router = Router()
-
-    log.startLogging(sys.stdout)
-
-    if config["server"]["secure"]:
-        factory = ChatFactory(router, "wss://" + config["server"]["host"] + ":" + str(config["server"]["port"]))
-    else:
-        factory = ChatFactory(router, "ws://" + config["server"]["host"] + ":" + str(config["server"]["port"]))
-    factory.protocol = ServerProtocol
-
-    reactor.listenTCP(config["server"]["port"], factory)
-    reactor.run()
