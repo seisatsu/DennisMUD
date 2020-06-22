b48d998721c04e98826ac6ece184e7a8dbea3298
Michael D. Reiley <seisatsu@seisat.su>
2020-06-22 13:52:06 -0700

    Better use of pgrep in run.sh, other improvements

--- run.sh
+++ run.sh
@@ -30,22 +30,26 @@
 # This is a quick and dirty script to run Dennis and restart in a couple seconds if it crashes,
 # while also keeping a log and PID file. It should work on any sane Linux/Unix system.
 # Usage: ./run.sh <start/restart/stop>
-# Background Usage: nohup ./run.sh <start/restart/stop> &
+# Background Start: nohup ./run.sh start &
 
+python3="python3"                      # Python3 executable name.
 syspath="/home/seisatsu/Source/Dennis" # The absolute path to the Dennis folder.
-sysuser="seisatsu"                     # The user Dennis will run as.
+sysuser="seisatsu"                     # Make sure we are running as this user, or else exit.
 logname="dennis.log"                   # The name of the log file, relative or absolute.
 pidfile="run.sh.pid"                   # File for storing this script's PID, relative or absolute.
 lckfile="dennis.lock"                  # Lock file so we don't run twice, relative or absolute.
-
-cd $syspath
-trap "rm -f $lckfile $pidfile && echo '\nRun script terminating.' && exit 0" 2 15
 
 # Check user.
 if [ "$(whoami)" != "$sysuser" ]; then
     echo "Script must be run as user: $sysuser"
     exit 1
 fi
+
+# Change to Dennis directory, or exit if impossible.
+cd "$syspath" || (echo "Could not cd to Dennis folder." && exit 1)
+
+# Trap SIGINT and SIGTERM to clean up lock file and pid file.
+trap 'rm -f $lckfile $pidfile && echo "\nRun script terminating." && exit 0' 2 15
 
 # Save PID of this script, enter infinite loop of starting Dennis if it exits,
 # and send the output to the log file. Make a lock file.
@@ -54,11 +58,11 @@ if [ "$1" = "start" ]; then
         echo "Lockfile exists. Is Dennis already running? Not starting."
         exit 1
     fi
-    touch $lckfile
+    touch "$lckfile"
     echo "Starting Dennis..."
-    echo $$ > $pidfile
+    echo $$ > "$pidfile"
     while true; do
-        python3 server.py 2>&1 | tee -a $logname
+        $python3 server.py 2>&1 | tee -a "$logname"
         echo "Dennis stopped, restarting in 2 seconds..."
         sleep 2
     done
@@ -68,26 +72,25 @@ fi
 # Restart Dennis by stopping it while leaving the parent script running.
 if [ "$1" = "restart" ]; then
     echo "Restarting Dennis..."
-    currpid=`cat $pidfile`
-    dnnspid=`pgrep -P $currpid | head -n1`
-    kill $dnnspid
+    currpid=$(cat "$pidfile")
+    dnnspid=$(pgrep -x -P "$currpid" "$python3")
+    kill "$dnnspid"
     exit 0
 fi
 
 # Stop Dennis by stopping it and the parent script. Remove the lockfile.
 if [ "$1" = "stop" ]; then
     echo "Stopping Dennis..."
-    currpid=`cat $pidfile`
-    dnnspid=`pgrep -P $currpid | head -n1`
-    kill $dnnspid
+    currpid=$(cat "$pidfile")
+    dnnspid=$(pgrep -x -P "$currpid" "$python3")
+    kill "$dnnspid"
     sleep 2
-    kill $currpid
-    rm -f $lckfile $pidfile # Most likely this already happened in the signal trap.
+    kill "$currpid"
+    rm -f "$lckfile" "$pidfile" # Most likely this already happened in the signal trap.
     exit 0
 fi
 
 # We didn't get a recognizable option.
 echo "Invalid or no arguments."
 echo "Usage: ./run.sh <start/restart/stop>"
-echo "Background Usage: nohup ./run.sh <start/restart/stop> &"
 exit 1
